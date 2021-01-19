baff986651728fd418e445b19b840c44364ec3b0
Lars Karbo <mail@larskarbo.no>
2021-01-19 20:08:15 +0100

    try this

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,3 +9,18 @@ exports.onCreatePage = async ({ page, actions }) => {
         createPage(page)
     }
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /peaks.js/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
